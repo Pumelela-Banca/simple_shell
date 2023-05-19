7595610b7a8f95fe0c6fb84d66e32280ee2d8088
Pumelela-Banca <pumelelabanca@gmail.com>
2023-05-19 11:08:36 +0200

    added free function for struct

---
+++ free_list.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *
+ * free_list - frees sruct with enviromental vvariables
+ *
+ * @head: first node pointer
+ *
+ * Return: Void
+ */
+
+void free_list(_var *head)
+{
+	_var *ptr = NULL;
+
+	while (head != NULL)
+	{
+		ptr = head;
+		head = head->next;
+		free(ptr->name);
+		free(ptr->value);
+		free(ptr);
+	}
+}
