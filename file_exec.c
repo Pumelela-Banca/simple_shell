7fafda3982e8ffb077a35a2525e7676238aca7d8
jules ntizimira <ntizimijules@gmail.com>
2023-05-19 22:04:49 +0000

    update file_exec

--- file_exec.c
+++ file_exec.c
@@ -4,11 +4,29 @@
  *  file_exec - exit
  *  @envp: *envp[]
  *  @cmds: *cmds[][]
+ *  @argv: argv
+ *  @count: count
+ *  Return: int
  */
-void file_exec(char *envp[], char *cmds[])
-{
-	pid_t pid = fork();
 
+int file_exec(char *envp[], char *cmds[], char *argv[], int *count)
+{
+	char *real = NULL, *tmp = NULL;
+	pid_t pid;
+
+	real = getpath(cmds[0], envp);
+	if (real == NULL)
+	{
+		print_error(count, argv, cmds);
+		_free(cmds);
+		return (1);
+	}
+	tmp = cmds[0];
+	cmds[0] = _strdup(real);
+	free(tmp);
+	free(real);
+
+	pid = fork();
 	if (pid == -1)
 	{
 		perror("fork failed");
@@ -27,4 +45,5 @@ void file_exec(char *envp[], char *cmds[])
 		wait(NULL);
 		_free(cmds);
 	}
+	return (0);
 }
