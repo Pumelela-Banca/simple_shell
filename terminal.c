06480f62b4c5b5f55291810f4fc114b88d777e57
Pumelela-Banca <pumelelabanca@gmail.com>
2023-05-24 22:44:33 +0200

    removed old if statements

--- terminal.c
+++ terminal.c
@@ -40,38 +40,11 @@ void terminal(char *argv[])
 			k = 0;
 			continue;
 		}
-		/*if (_strcmp(cmds[0], "exit") == 0)
-			_exit_(cmds);*/
-		/*else if (_strcmp(cmds[0], "clear") == 0)
-		{
-			_clear(cmds);
-			continue;
-		}
-		else if (_strcmp(cmds[0], "cd") == 0)
-		{
-			_cd(cmds);
-			continue;
-		}*/
 		else if (_strcmp(cmds[0], "which") == 0)
 		{
 			_which(cmds);
 			continue;
 		}
-		/*else if (_strcmp(cmds[0], "env") == 0)
-		{
-			_env(cmds);
-			continue;
-		}
-		else if (_strcmp(cmds[0], "setenv") == 0)
-		{
-			_setenv(cmds);
-			continue;
-		}
-		else if (_strcmp(cmds[0], "unsetenv") == 0)
-		{
-			_unsetenv(cmds);
-			continue;
-		}*/
 		else if ((_strcmp(cmds[0], "echo") == 0) &&
 				(cmds[1][0] == '$'))
 		{
